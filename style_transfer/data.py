b052cb3a65d874f036bf0cbe2c1b98849e5748c3
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2025-01-10 20:51:18 +0800

    Add scale image numpy

--- style_transfer/data.py
+++ style_transfer/data.py
@@ -6,6 +6,8 @@ from typing import List, Tuple
 import torch
 from torchvision import transforms
 from PIL import Image
+import cv2
+import numpy as np
 from style_transfer.utils.func_utils import my_check_return_type
 
 
@@ -37,7 +39,7 @@ def normalize_img_tensor(
     """将图像张量标准化
 
     Args:
-        image_tensor (torch.Tensor): 图像张量
+        image_numpy (np.ndarray): 图像数组
         mean (List[float]): 均值
         std (List[float]): 标准差
 
@@ -128,3 +130,84 @@ def resize_img_tensor(
         ]
     )
     return transform(image_tensor)
+
+
+def size_greater_than_or_equal_to_required(
+    image_numpy: np.ndarray, size: Tuple[int, int]
+) -> bool:
+    """判断图像张量尺寸是否大于等于要求的尺寸"""
+    return image_numpy.shape[-2] >= size[0] or image_numpy.shape[-1] >= size[1]
+
+
+def scale_img_numpy(image_numpy: np.ndarray, size: Tuple[int, int]):
+    """缩放图像张量
+
+    Args:
+        image_tensor (np.ndarray): 图像张量
+        size (Tuple[int, int]): 尺寸
+
+    Returns:
+        np.ndarray: 缩放后的图像张量
+        function: 恢复尺寸的函数
+    """
+
+    # pylint: disable=no-member
+    # Pylint 似乎无法识别 cv2 的属性，因此这里禁用了这个检查
+    if size[0] <= 0 or size[1] <= 0:
+        raise cv2.error("Invalid size")
+    gaussian_pyramid = [image_numpy]
+    while size_greater_than_or_equal_to_required(image_numpy=image_numpy, size=size):
+        print(image_numpy.shape)
+        image_numpy = cv2.pyrDown(image_numpy)
+        gaussian_pyramid.append(image_numpy)
+        print(image_numpy.shape)
+
+    if len(gaussian_pyramid) <= 1:
+        return cv2.resize(image_numpy, size), lambda x: cv2.resize(
+            x, image_numpy.shape[0:2]
+        )
+    gaussian_pyramid.pop()
+
+    laplacian_pyramid = []
+
+    for i in range(len(gaussian_pyramid) - 1, 0, -1):
+        expand = cv2.pyrUp(gaussian_pyramid[i])
+        laplacian = cv2.subtract(gaussian_pyramid[i - 1], expand)
+        laplacian_pyramid.append(laplacian)
+
+    def restore_size(processed_image_numpy: np.ndarray):
+        """恢复尺寸"""
+        if gaussian_pyramid[-1].shape[0:2] != processed_image_numpy.shape[0:2]:
+            processed_image_numpy = cv2.resize(
+                processed_image_numpy, gaussian_pyramid[-1].shape[0:2]
+            )
+        for laplacian in laplacian_pyramid:
+            processed_image_numpy = cv2.pyrUp(processed_image_numpy)
+            processed_image_numpy = cv2.add(processed_image_numpy, laplacian)
+        return processed_image_numpy
+
+    if gaussian_pyramid[-1].shape[0:2] != size:
+        image_numpy = cv2.resize(gaussian_pyramid[-1], size)
+    else:
+        image_numpy = gaussian_pyramid[-1]
+
+    return image_numpy, restore_size
+
+
+def scale_img_tensor(image_tensor: torch.Tensor, size: Tuple[int, int]):
+    """缩放图像张量
+
+    Args:
+        image_tensor (torch.Tensor): 图像张量
+        size (Tuple[int, int]): 尺寸
+
+    Returns:
+        torch.Tensor: 缩放后的图像张量
+        function: 恢复尺寸的函数
+    """
+    image_numpy = image_tensor.numpy().transpose(1, 2, 0)
+    image_numpy, restore_size = scale_img_numpy(image_numpy, size)
+    image_tensor = torch.from_numpy(image_numpy.transpose(2, 0, 1))
+    return image_tensor, lambda x: torch.from_numpy(
+        restore_size(x.numpy().transpose(1, 2, 0))
+    ).transpose(2, 0, 1)
